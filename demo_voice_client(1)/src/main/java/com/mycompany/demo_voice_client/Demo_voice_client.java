b60d73db3a4f75666650fce45332df2aa4956210
Hoàng Khương Duy <102210204@sv1.dut.udn.vn>
2023-10-06 21:44:32 +0700

    update

---
+++ demo_voice_client(1)/src/main/java/com/mycompany/demo_voice_client/Demo_voice_client.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo_voice_client;
+
+/**
+ *
+ * @author ASUS-PRO
+ */
+public class Demo_voice_client {
+
+  public static boolean calling = false;
+    public static void main(String[] args) {
+        client_form form = new client_form();
+        form.setVisible(true);
+    }
+}
