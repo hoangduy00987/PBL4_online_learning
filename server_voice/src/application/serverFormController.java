0a784ce767982ad66bf2c5fd0448b7cac1cdaa9e
Hoàng Khương Duy <102210204@sv1.dut.udn.vn>
2023-11-25 11:29:17 +0700

    update 3.0

--- server_voice/src/application/serverFormController.java
+++ server_voice/src/application/serverFormController.java
@@ -1,11 +1,14 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.concurrent.CountDownLatch;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -15,16 +18,12 @@ import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-public class clientFormController {
-	public TargetDataLine audio_in;
+public class serverFormController {
 	public SourceDataLine audio_out;
+	public TargetDataLine audio_in;
 	public DatagramSocket socket;
 	public DatagramPacket packet;
 	byte buffer[] = new byte[11000];
-	public String ip_server = "10.10.59.172";
 	public int port = 9999;
 	public void out_audio() {
 	    try {
@@ -41,23 +40,22 @@ public class clientFormController {
 	                audio_out.open(format);
 	                audio_out.start();
 
-	               client_thread thr = new client_thread();
-				   client_form.Calling = true;
+	               server_thread thr = new server_thread();
+	               server_form.Calling = true;
 	               thr.socket = new DatagramSocket(port);
 	               thr.audio_out = audio_out;
 	               thr.start();
-	              speaker.setVisible(false);
+	               
 	    } catch (Exception ex) {
 	      
 	    }
 	   }
-	   
-     Thread client_send =  new Thread( ()-> {
-    	 
-    	 try { 
-    		 // Khởi tạo định dạng âm thanh
+	Thread server_send =  new Thread(()-> {
+   	 
+   	 try { 
+   		 // Khởi tạo định dạng âm thanh
 	        AudioFormat format = new AudioFormat(44100.0F,16,2,true,false);
-    	// Tạo thông tin cho dòng đầu vào âm thanh (TargetDataLine)
+   	     // Tạo thông tin cho dòng đầu vào âm thanh (TargetDataLine)
 	        DataLine.Info infor = new DataLine.Info(TargetDataLine.class, format);
 
 	        // Kiểm tra xem hệ thống có hỗ trợ định dạng âm thanh được chỉ định hay không
@@ -65,21 +63,23 @@ public class clientFormController {
 	            System.out.println("Không hỗ trợ định dạng âm thanh này");
 	            return;
 	        }
-	            
+
 	        // Tạo và cấu hình dòng đầu vào âm thanh (TargetDataLine)
 	           
 					audio_in = (TargetDataLine) AudioSystem.getLine(infor);
 					audio_in.open(format);
 					audio_in.start();
-					System.out.print("Gửi âm thanh đến server");
+					System.out.print("Gửi âm thanh đến client");
 					while(true) {
 					//đọc dữ liệu âm thanh để lưu vào buffer
 					audio_in.read(buffer, 0, buffer.length);
 					socket = new DatagramSocket();
 					//đối tượng datagrampacke t để gửi âm thanh
-					InetAddress ip = InetAddress.getByName(ip_server);
-					DatagramPacket data = new DatagramPacket(buffer, buffer.length,ip,port);
-					socket.send(data);
+					 for(DatagramPacket value : Singleton.getInstance().getUsers()) {
+					      DatagramPacket voice_server = new DatagramPacket(buffer, buffer.length, value.getAddress(), value.getPort());
+					      socket.send(voice_server);
+					    
+					 }
 					}
 					
 				} catch (LineUnavailableException | UnknownHostException | SocketException e) {} catch (IOException e) {
@@ -91,23 +91,26 @@ public class clientFormController {
 		         audio_in.stop();
 		         audio_in.close();
 
-		
-     });
-     @FXML
-     private Button mic;
+    });
+	
+    @FXML
+    private Button mic;
 
-     @FXML
-     private Button speaker;
+    @FXML
+    private Button speaker;
 
-     @FXML
-     void btn_mic(ActionEvent event) {
-    	 client_send.start();
-    	 mic.setVisible(false);
-     }
+    @FXML
+    void btn_mic(ActionEvent event) {
+    server_send.start();
+    mic.setVisible(false);
+    }
 
-     @FXML
-     void btn_speaker(ActionEvent event) {
-    	 out_audio();
-     }
+    @FXML
+    void btn_speaker(ActionEvent event) {
+    	out_audio();
+    	speaker.setVisible(false);
+    }
 
+
+	
 }
